044d2d287ba438624a126c5150c0da6b4d3f3d9b
MAG-BOSS <b18025@students.iitmandi.ac.in>
2020-12-27 14:34:13 +0530

    completed basic app with voice control functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,71 @@
-import React , {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
-import NewsCards from './components/NewsCards/NewsCards.js'
 
-const alanKey = '82ea6583b48aee86c1cbfe317c02cfb82e956eca572e1d8b807a3e2338fdd0dc/stage';
+import logo from './images/logo.png';
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
 
-const App = () =>{
-    const [newsArticles , setNewsArticles] = useState([]);
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
-    useEffect(()=>{
-        alanBtn({
-            key: alanKey,
-            onCommand: ({command, articles})=>{
-                if(command === 'newHeadlines'){
-                    console.log(articles);
-                }
-            }
-        })
-    },[])
+  const classes = useStyles();
 
-    return(
-        <div>
-            <h1>Alan AI News App</h1>
-            <NewsCards articles={newsArticles} />
+  useEffect(() => {
+    alanBtn({
+      key: '82ea6583b48aee86c1cbfe317c02cfb82e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a className={classes.link} href="#"> Shikhar Kushwah</a>
+          </Typography>
+          <img className={classes.image} src={logo} height="50px"  />
         </div>
-    );
-}
+      ) : null}
+    </div>
+  );
+};
 
 export default App;
